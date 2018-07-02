6a3319a36a3855bae2e008031a43f445013e6600
Harsh Vyas <hhvys09@gmail.com>
2018-07-02 17:24:38 +0530

    changed persistedState

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,16 +1,13 @@
 import {throttle} from 'lodash';
-import {loadState, parseStringifiedDate, saveState} from "./localStorage";
+import {loadState, saveState} from "./localStorage";
 import todoApp from "./reducers";
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {STATE_WITH_TABS} from "./reducers/__test__/__fixtures__/reducers.fixtures";
-import {getTabsWithInfo} from "./reducers/tabs/tabs";
 // import persistedState from '../performance/stateGenerator';
 
 const configureStore = () => {
-	const persistedState = parseStringifiedDate(STATE_WITH_TABS);
-	// console.log(persistedState);
+	const persistedState = loadState();
 	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 	const store = createStore(
 		todoApp,
